b2b9c83105fdeb6eb4e4bbe7d42c7fae8c53d1e4
Raphael Knecht <Raphael@MACBOOKAIR-0AF1.local>
2025-05-11 13:55:37 +0200

    example output

--- scripts/race_comp_pipeline.R
+++ scripts/race_comp_pipeline.R
@@ -52,7 +52,7 @@ yaml_file <- if (interactive()) {
 params <- load_param(here::here(yaml_file))
 
 # if you want to remove certain files
-params$fit_data <- params$fit_data[-1]
+# params$fit_data <- params$fit_data[-1]
 
 
 # Read and process FIT files
